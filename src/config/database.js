a1a11705ee9a8dadaf17706078ccd09490eb95db
Sumit Bhumbak <skbhumbak@gmail.com>
2024-05-20 19:18:48 +0530

    db

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  "tasks",
+  "task_7sd5",
   "task_7sd5_user",
   "d8SUqCK9By6Ysxz1dWVL2DjVBhdoSZTq",
   {
-    host: "dpg-cp5l2h779t8c73f0ab70-a:5432",
+    host: "dpg-cp5l2h779t8c73f0ab70-a",
     dialect: "postgres",
   }
 );
