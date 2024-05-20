7aed7700d84813689121fbfaa8bfa9d6c39e384c
Sumit Bhumbak <skbhumbak@gmail.com>
2024-05-20 19:16:21 +0530

    postgress port

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   "task_7sd5_user",
   "d8SUqCK9By6Ysxz1dWVL2DjVBhdoSZTq",
   {
-    host: "dpg-cp5l2h779t8c73f0ab70-a",
+    host: "dpg-cp5l2h779t8c73f0ab70-a:5432",
     dialect: "postgres",
   }
 );
